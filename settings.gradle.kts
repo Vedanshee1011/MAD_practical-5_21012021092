87884f37fcfb728a30922cdcf41d0aeefc80c03e
Himanshu <modihimanshu880@gmail.com>
2023-11-03 18:13:29 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD_practical-5_21012021092"
+include(":app")
+ 
